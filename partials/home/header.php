dce03221f9340784b9619bf7754100e7c881dc55
Doonguin <penguinicesp@gmail.com>
2023-03-21 15:47:43 +0100

    Header zo goed als gefixt

--- partials/home/header.php
+++ partials/home/header.php
@@ -18,6 +18,17 @@
 		<div class="header-content">
 			<?php if($headerSubtitle != ''): ?><p class="header-subtitle"><?php echo $headerSubtitle; ?></p><?php endif; ?>
 			<?php if($headerTitle != ''): ?><h3 class="header-title"><?php echo $headerTitle; ?></h3><?php endif; ?>
+			<!-- TYGO'S WERK -->
+			<p>
+				Sample text Sample text Sample text Sample text <br>
+				Sample text Sample text Sample text Sample text <br>
+				Sample text Sample text Sample text Sample text 
+			</p>
+
+			<button class="header-button">
+				Boek je feestje!
+			</button>
+			<!-- TYGO'S WERK -->
 			<?php if( $headerLink != '' ): 
 	            $link_url = $headerLink['url'];
 	            $link_title = $headerLink['title'];
@@ -32,6 +43,10 @@
 			<?php if($headerSideSubtitle != ''): ?><p class="header-vriendenvan-subtitle"><?php echo $headerSideSubtitle; ?></p><?php endif; ?>
 			<?php if($headerSideTitle != ''): ?><h3 class="header-vriendenvan-title"><?php echo $headerSideTitle; ?></h3><?php endif; ?>
 			<?php if($headerSideText != ''): ?><p><?php echo $headerSideText; ?></p><?php endif; ?>
+			<!-- TYGO'S WERK -->
+			<p>Alle openings tijden <span style="text-decoration: underline;">bekijken</span></p>
+			<h3 style="margin-top: 100px;">Activiteiten</h3>
+			<!-- TYGO'S WERK -->
 			<div class="btn-holder">
 				<?php if( $headerSideLink != '' ): 
 	            $link_url = $headerSideLink['url'];
@@ -48,7 +63,14 @@
 	          ?>
 	            <a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
 	        <?php endif; ?>
+				<!-- TYGO'S WERK -->
+				<a class="btn" href="#" target="_self">Schoolreisje</a>
+				<a class="btn" href="#" target="_self">BSO of groep</a>
+				<!-- TYGO'S WERK -->
 			</div>
+			<!-- TYGO'S WERK -->
+			<p>Alle activiteiten <span style="text-decoration: underline;">bekijken</span></p>
+			<!-- TYGO'S WERK -->
 		</div>
 	</div>
 </header>
